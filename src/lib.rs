190fa56124b3b5118b69be830cded6ecf665eae7
dewo <marefimoghaddam@unixerr.com>
2023-01-15 10:44:45 +0330

    add sources

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 
 
+
+// https://bparli.medium.com/adventures-in-rust-and-load-balancers-73a0bc61a192
+// https://github.com/jsdw/weave
+// https://github.com/hyperium/hyper/blob/master/examples/http_proxy.rs
+// https://github.com/hyperium/hyper/blob/master/examples/gateway.rs
+// https://dzone.com/articles/rust-based-load-balancing-proxy-server-with-async
+// https://truelayer.com/blog/grpc-load-balancing-in-rust
+// https://medium.com/load-balancer-series/writing-a-http-load-balancer-in-python-using-tdd-theoretical-concepts-fb6dab3e879b
+// https://kemptechnologies.com/load-balancer/load-balancing-algorithms-techniques
+
 // proxy, firewall, vpns, packet sniffer and load balancer like pingora, HAproxy, v2ray and wireshark for all layers
 // • tokio, zmq and riker actor
 // • v2ray and tor protocols
