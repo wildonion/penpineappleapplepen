068abbf0a7cb647db4fbbf5618d1d0482a530d47
dewo <marefimoghaddam@unixerr.com>
2023-02-07 13:55:36 +0330

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 
 
 
-// proxy, firewall, vpns, packet sniffer and load balancer like pingora, HAproxy, v2ray and wireshark for all layers
+// proxy, firewall, vpns, packet sniffer and load balancer like pingora, nginx, HAproxy, v2ray and wireshark for all layers
 // • tokio, zmq and riker actor
 // • a p2p based vpn like v2ray and tor using noise protocol
 // • decompress encoded packet 
