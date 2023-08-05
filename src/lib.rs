d4e6e884fb748a3d1034249b0c47e98a9aadacaf
dewo <abarmardeatashyne@gmail.com>
2023-08-05 14:35:31 +0330

    add a bpf loader

--- src/lib.rs
+++ src/lib.rs
@@ -133,3 +133,23 @@ pub struct Pod{ //// a pod is a load balancer which can have one or more contain
     pub id: String,
     pub containers: Vec<Container>,
 }
+
+
+/*
+    with BPF VM we can compile the whole node 
+    into an .elf or .so which contains the 
+    BPF bytecode that can be executed from 
+    the linux kernel. LLVM13 is needed 
+    to compile BPF bytecode.
+*/
+pub async fn bpf_loader(){
+
+    // https://cheats.rs/#behind-the-scenes
+    // https://blog.redsift.com/labs/writing-bpf-code-in-rust/
+    // bpf loader
+    // ... 
+    
+}
+
+
+
