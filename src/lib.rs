620604ab02571626d8f3c9209a6638585b274490
dewo <abarmardeatashyne@gmail.com>
2023-08-23 10:16:09 +0330

    better exp of what we're trying to do

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@
 
 // a realtime node monitoring and packet sniffing tools
 // using zmq to manage the load of each instance 
-// in realtime, in our proxy zmq subscribers are node 
-// instances that must be balanced 
+// in realtime, in our proxy, zmq subscribers are server app 
+// node instances that must be balanced by subscribing 
+// on the incoming topic from the balancer publishers
 
 // we can build zmq using tokio socket actors 
 // and build libp2p and rpc system 
@@ -39,7 +40,7 @@
 // ngrok process: [https://docs.rs/ngrok/latest/ngrok/] || [https://ngrok.com/docs/using-ngrok-with/rust/]
 //  ➙ first it'll open a port on local machine 
 //  ➙ then it will create a session on that port with a random dns on its servers 
-//  ➙ finally it forwars all the traffic to that session to the local port it created
+//  ➙ finally it forwards all the traffic to that session to the local port it created
 
 
 // with simple-hyper-server-tls, tokio-rustls and noise-protocol we can create a secured communication 
@@ -50,7 +51,7 @@
 
 // ➙ we can setup exit codes with enum to know which error caused the program to stopped when using Box<dyn Error> which can be implemented for the type that will cause the error at runtime 
 // ➙ public key digital signature ring ed25519 verification for updating app and server verification apis 
-// ➙ proxy, firewall, vpns, packet sniffer and load balancer like pingora, docker networking, nginx, ngrok, HAproxy, v2ray and wireshark for all layers
+// ➙ bpf based proxy, firewall, vpns, packet sniffer and load balancer like pingora, docker networking, nginx, ngrok, HAproxy, v2ray and wireshark for all layers
 //    • tokio channels + worker green threadpool + event loopg, hyper, riker actor concepts, rpc capnp, zmq, libp2p stacks, ws, tcp and udp
 //    • a p2p based vpn like v2ray and tor using noise protocol, gossipsub, kademlia quic and p2p websocket 
 //    • simple-hyper-server-tls, noise-protocol and tokio-rustls to implement ssl protocols and make a secure channel for the underlying raw socket streams
@@ -148,7 +149,7 @@ pub struct Pod{ //// a pod is a load balancer which can have one or more contain
     into an .elf or .so which contains the 
     BPF bytecode that can be executed from 
     the linux kernel. LLVM13 is needed 
-    to compile BPF bytecode.
+    to compile BPF bytecode for Rust version
 */
 pub async fn bpf_loader(){
 
