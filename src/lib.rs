e23f04059f3f1f8b787858ac98e4f23cc171da30
dewo <abarmardeatashyne@gmail.com>
2023-08-23 13:09:51 +0330

    add bpf module

--- src/lib.rs
+++ src/lib.rs
@@ -144,17 +144,22 @@ pub struct Pod{ //// a pod is a load balancer which can have one or more contain
 }
 
 
-/*
-    with BPF VM we can compile the whole node 
-    into an .elf or .so which contains the 
-    BPF bytecode that can be executed from 
-    the linux kernel. LLVM13 is needed 
-    to compile BPF bytecode
-*/
 pub mod bpf(){
 
-    // https://cheats.rs/#behind-the-scenes
-    // https://blog.redsift.com/labs/writing-bpf-code-in-rust/
+
+    /* 
+    
+        with BPF VM we can compile the whole node 
+        into an .elf or .so which contains the 
+        BPF bytecode that can be executed from 
+        the linux kernel. LLVM13 is needed 
+        to compile BPF bytecode for Rust version
+    
+        https://blog.redsift.com/labs/writing-bpf-code-in-rust/
+        binding using .so and https://crates.io/crates/pyo3
+
+    */
+    
     // bpf loader
     // ... 
     
